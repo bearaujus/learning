7c383aae0941199b26fab739b5cbd644fddb129a
BearAuJus <haryobagasasyafah6@gmail.com>
2021-09-04 17:30:20 +0700

    learning 10

---
+++ php/2_oop_php/l10/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Declaring Constant Via Define
+define('NAME', 'Haryo Bagas Assyafah');
+
+// Declaring Constant Via Constant
+const NRP = '1773031';
+
+// Using Constant
+echo NRP . ' - ' . NAME;
