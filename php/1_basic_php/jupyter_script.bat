5a69c40417ff81badf85ccca2569db3f8ce8dd2d
BearAuJus <haryobagasasyafah6@gmail.com>
2021-09-08 17:23:09 +0700

    Update README.md

---
+++ php/1_basic_php/jupyter_script.bat
@@ -0,0 +1,2 @@
+%USERPROFILE%\anaconda3\python.exe "%USERPROFILE%"\anaconda3\cwp.py %USERPROFILE%\anaconda3 %USERPROFILE%\anaconda3\python.exe "%USERPROFILE%"\anaconda3\Scripts\jupyter-notebook-script.py "%cd%"
+pause
